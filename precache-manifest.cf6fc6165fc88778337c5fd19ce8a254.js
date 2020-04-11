16c1526d2900652341e7b47f1cb384fd092153b7
trinhmaingoc <trinhmn.dlhp@gmail.com>
2020-04-12 00:40:13 +0700

    Updates

---
+++ precache-manifest.cf6fc6165fc88778337c5fd19ce8a254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebc3674ccdd46932fc0cc1dad143cbd",
+    "url": "/lesson11-training-redux/index.html"
+  },
+  {
+    "revision": "afd1470004393749917a",
+    "url": "/lesson11-training-redux/static/css/main.38544fcc.chunk.css"
+  },
+  {
+    "revision": "fc1133269a79cf6e9760",
+    "url": "/lesson11-training-redux/static/js/2.7f8206a1.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/lesson11-training-redux/static/js/2.7f8206a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd1470004393749917a",
+    "url": "/lesson11-training-redux/static/js/main.a868ef12.chunk.js"
+  },
+  {
+    "revision": "e24dba519e23e5869187",
+    "url": "/lesson11-training-redux/static/js/runtime-main.e7a1ab17.js"
+  }
+]);
